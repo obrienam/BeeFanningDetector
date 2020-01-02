e64eb8e00b7babd1dd2ab93a2f966b9c321aef28
Aidan O'Brien <obrienam@appstate.edu>
2020-01-02 13:53:17 -0500

    Got contour detection to work

--- Files/stationary_detector.py
+++ Files/stationary_detector.py
@@ -17,14 +17,19 @@ while True:
     if firstFrame is not None:
         img1=firstFrame
         img2=frame
-        bk = cv2.imread('Assets/background-2.jpg')
+        bk = cv2.imread('Assets/bee-background.png')
         subImage1=(bk.astype('int32')-img1.astype('int32')).clip(0).astype('uint8')
         grey1=cv2.cvtColor(subImage1,cv2.COLOR_BGR2GRAY)
         retval1,thresh1=cv2.threshold(grey1,35,255,cv2.THRESH_BINARY_INV)
         img1=grey1
+        thresh3=thresh1
+        im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(firstFrame, contours, -1, (0,255,0), 3)
         cv2.imshow("thresh",thresh1)
-        im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.imshow("thresh2",firstFrame)
         
+       
+
 
         subImage2=(bk.astype('int32')-img2.astype('int32')).clip(0).astype('uint8')
         grey2=cv2.cvtColor(subImage2,cv2.COLOR_BGR2GRAY)
