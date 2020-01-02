1d2c1f7e8c4ac2cd51d428ccb52ae3799e9ae8fc
Aidan O'Brien <obrienam@appstate.edu>
2020-01-02 14:57:43 -0500

    Started experimenting with filling contours

--- Files/stationary_detector.py
+++ Files/stationary_detector.py
@@ -1,50 +1,56 @@
 import cv2
+import numpy as np
 vs=cv2.VideoCapture("/Users/aidanobrien/Documents/GitHub/BeeFanningDetector/Assets/bees2.mp4")
-#vs=cv2.VideoCapture("C:/Users/obrienam/Documents/GitHub/CV_Research/Assets/contrast.mp4")
-firstFrame=None
-prev_s=None
-bev="Steady"
-s=0
+
+img1=None
 frame_width = int(vs.get(3))
 frame_height = int(vs.get(4))
 
+def rem_movement(thresh,cnt1,cnt2):
+    cv2.drawContours(thresh, cnt1, -1, (255,255,255), -1)
+    cv2.imshow("thresh",thresh)
+    return thresh
 while True:
-    hasFrames,frame=vs.read()
- #   hasFrames2,oframe=fs.read()
+    hasFrames,img2=vs.read()
     if (hasFrames==False):
         break
     
-    if firstFrame is not None:
-        img1=firstFrame
-        img2=frame
+    if img1 is not None:
+
+        #take first threshold
         bk = cv2.imread('Assets/bee-background.png')
         subImage1=(bk.astype('int32')-img1.astype('int32')).clip(0).astype('uint8')
         grey1=cv2.cvtColor(subImage1,cv2.COLOR_BGR2GRAY)
         retval1,thresh1=cv2.threshold(grey1,35,255,cv2.THRESH_BINARY_INV)
-        img1=grey1
-        thresh3=thresh1
-        im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(firstFrame, contours, -1, (0,255,0), 3)
-        cv2.imshow("thresh",thresh1)
-        cv2.imshow("thresh2",firstFrame)
+
+        #find first img2 contours
+        im2, contours1, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img1, contours1, -1, (0,255,0), 3)
         
-       
-
-
+        #find second threshold
         subImage2=(bk.astype('int32')-img2.astype('int32')).clip(0).astype('uint8')
         grey2=cv2.cvtColor(subImage2,cv2.COLOR_BGR2GRAY)
         retval2,thresh2=cv2.threshold(grey2,35,255,cv2.THRESH_BINARY_INV)
         
-        img2=grey2
-        firstFrame=frame
+        #find second img2 contours
+        im3, contours2, hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        thresh1=rem_movement(thresh1,contours1,contours2)
+        #show treshold and video with contours
+        
+        cv2.imshow("contours",img1)
+
+        #increment img2
+        img1=img2
+
     else:    
-        firstFrame=frame
+        img1=img2
     key=cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
         break
     
     
-    cv2.imshow("vid",frame)
+    cv2.imshow("vid",img2)
 vs.release()
 cv2.destroyAllWindows()
