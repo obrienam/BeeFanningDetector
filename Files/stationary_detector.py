65d05ff895670a239513d8f163a6266866170e99
Aidan O'Brien <obrienam@appstate.edu>
2020-01-03 18:16:58 -0500

    Added documentation for clarity

--- Files/stationary_detector.py
+++ Files/stationary_detector.py
@@ -7,21 +7,25 @@ frame_width = int(vs.get(3))
 frame_height = int(vs.get(4))
 
 def rem_movement(thresh,cnt1,cnt2):
-    
+    #loop through first conotur list
     for c1 in cnt1:
         found=False
-        cntstill=[]
+        cntmoving=[]
+        #loop through second contour list
         for c2 in cnt2:
+            #match the contours
             m=cv2.matchShapes(c1,c2,2,0.0)
             if m==0.0:
+                #if match, stop searching
                 print("match")
                 found=True
                 break
         if(found==False):
             print("not found")
-            cntstill.append(c1)
-    cv2.drawContours(thresh, cntstill, -1, (255,255,255), -1)
-    #cv2.imshow("thresh",thresh)
+            #if not found, append it to moving contours
+            cntmoving.append(c1)
+    #fill contours that moved with white
+    cv2.drawContours(thresh, cntmoving, -1, (255,255,255), -1)
     return thresh
 while True:
     hasFrames,img2=vs.read()
@@ -47,8 +51,10 @@ while True:
         
         #find second img2 contours
         im3, contours2, hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
+
+        #remove moved contours
         thresh1=rem_movement(thresh1,contours1,contours2)
+        
         #show treshold and video with contours
         cv2.imshow("threshold",thresh1)
         cv2.imshow("contours",img1)
