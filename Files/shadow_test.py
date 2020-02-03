1e9bf5bf65064f0b0cadf306d45d2b7696c62dcb
Aidan O'Brien <obrienam@appstate.edu>
2020-02-03 11:41:46 -0500

    Experimented with watershed algorithm

--- Files/shadow_test.py
+++ Files/shadow_test.py
@@ -10,8 +10,33 @@ def to_thresh(img,bk):
     thresh=cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel)
     return thresh
 
-img = cv2.imread("C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/ref_3.jpg")
+img = cv2.imread("C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/coins.png")
 bk = cv2.imread("C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/bk_3_2.jpg")
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh=cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+# sure background area
+sure_bg = cv2.dilate(opening,kernel,iterations=3)
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+cv2.imwrite('C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/fg_test.png', sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+# Marker labelling
+ret, markers = cv2.connectedComponents(thresh)
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [0,0,255]
+cv2.imwrite('C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/img_test.png', img)
+cv2.imwrite('C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/bg_test.png', sure_bg)
 '''
 rgb_planes = cv2.split(img)
 
@@ -32,7 +57,7 @@ cv2.imwrite('shadows_out.jpg', result)
 cv2.imwrite('shadows_out_norm.jpg', result_norm)
 img=cv2.imread('/Users/aidanobrien/Documents/GitHub/BeeFanningDetector/Assets/shadows_out_norm.jpg')
 bk=cv2.imread('/Users/aidanobrien/Documents/GitHub/BeeFanningDetector/Assets/white_bk.jpg')
-'''
+
 thresh=to_thresh(img,bk)
 im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, contours, 0, (0,255,0), 3)
@@ -41,3 +66,4 @@ print(contours[0].size)
 #thresh=to_thresh(img,bk)
 cv2.imwrite('C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/thresh_3.jpg', thresh)
 cv2.imwrite('C:/Users/obrienam/Documents/GitHub/BeeFanningDetector/Assets/fan_ref/img_cnt_test.jpg', img)
+'''
